1aba051c6ac5ae0ed4df13986393ad2507500ef9
seovacmarko5 <seovacmarko5@gmail.com>
2023-01-18 19:50:44 +0100

    working on header--typing text

--- src/components/head/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css'
-
-const Head = () => {
-    return (
-        <h1>marko</h1>
-    )
-}
-
-export default Head
---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className='head-typing'>
+                <div className="typing name"></div>
+                <div className="typing surname"></div>
+                <div className="typing born"></div>
+                <div className="typing profession"></div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
