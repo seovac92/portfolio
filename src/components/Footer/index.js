1e4ebe2fed490f22a31327a38dde4168efa38d62
seovacmarko5 <seovacmarko5@gmail.com>
2023-01-30 16:13:40 +0100

    making footer with contact

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,14 @@
+import './index.css'
+
 const Footer = () =>{
     return (
-        <h1>Footer</h1>
+        <footer>
+            <h2>&gt; Contact &lt;</h2>
+            <div className="footer-container">
+                <a href="tel:+381606005679">Tel: +381 60/6005679</a>
+                <a href="mailto:seovacmarko5@gmail.com">seovacmarko5@gmail.com</a>
+            </div>
+        </footer>
     )
 }
 
